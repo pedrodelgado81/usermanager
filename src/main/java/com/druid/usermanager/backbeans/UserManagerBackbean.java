f5c8b3eff67b6bc823e0338b7c275aaf2092412e
Pedro <pedrodelgado81@gmail.com>
2022-08-30 14:40:29 +0200

    Añadida validación por edad

--- src/main/java/com/druid/usermanager/backbeans/UserManagerBackbean.java
+++ src/main/java/com/druid/usermanager/backbeans/UserManagerBackbean.java
@@ -2,6 +2,9 @@ package com.druid.usermanager.backbeans;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -11,7 +14,6 @@ import java.util.regex.Pattern;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
-import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.faces.view.ViewScoped;
@@ -30,7 +32,9 @@ public class UserManagerBackbean implements Serializable {
 	private UserService userService;
 
 	private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
-	private static final Pattern pattern = Pattern.compile(PASSWORD_REGEX);
+	private static final String NAME_REGEX = "^[a-zA-Z\\º\\ª\\ñ\\Ñ\\- ]+$";
+	private static final Pattern passwpordPattern = Pattern.compile(PASSWORD_REGEX);
+	private static final Pattern namePattern = Pattern.compile(NAME_REGEX);
 
 	private List<User> usersList;
 
@@ -50,13 +54,23 @@ public class UserManagerBackbean implements Serializable {
 	private void init() {
 		usersList = this.userService.listUsers();
 	}
-	
-	public void addUser() {		
-		if(this.userService.findUserByEmail(this.email)!=null) {
-			FacesMessage msg = new FacesMessage("Usuario existente", "Ya existe un usuario en el sistema con ese email");
+
+	public void addUser() {
+
+		if (!isNameFormat(this.name) || !isNameFormat(this.surname)) {
+			FacesMessage msg = new FacesMessage("Formato Inválido",
+					"El nombre o apellidos contienen caracteres no válidos");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			return;
+		}
+
+		if (this.userService.findUserByEmail(this.email) != null) {
+			FacesMessage msg = new FacesMessage("Usuario existente",
+					"Ya existe un usuario en el sistema con ese email");
 			msg.setSeverity(FacesMessage.SEVERITY_WARN);
-			FacesContext.getCurrentInstance().addMessage(null,msg);
-		}else {
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		} else {
 			User newUser = new User();
 			newUser.setNombre(this.name);
 			newUser.setApellidos(this.surname);
@@ -68,29 +82,54 @@ public class UserManagerBackbean implements Serializable {
 			this.userService.persistUser(newUser);
 			FacesMessage msg = new FacesMessage("Usuario guardado", "Usuario añadido correctamente");
 			msg.setSeverity(FacesMessage.SEVERITY_INFO);
-			FacesContext.getCurrentInstance().addMessage(null,msg);
-			this.email=null;
-			this.password=null;
-			this.name=null;
-			this.surname=null;
-			this.birthDate=null;
-			this.role=null;
-			//Lo añadimos directamente a la lista para evitar lanzar una consulta adicional para el refresco
-			//En caso de ser una aplicacion con varios usuarios concurrentes relanzariamos la consulta (habria que ver cómo afecta al rendimiento si hay mucha concurrencia)
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			this.email = null;
+			this.password = null;
+			this.name = null;
+			this.surname = null;
+			this.birthDate = null;
+			this.role = null;
+			// Lo añadimos directamente a la lista para evitar lanzar una consulta adicional
+			// para el refresco
+			// En caso de ser una aplicacion con varios usuarios concurrentes relanzariamos
+			// la consulta (habria que ver cómo afecta al rendimiento si hay mucha
+			// concurrencia)
 			usersList.add(newUser);
 		}
 	}
-	
-	public void passwordValidator(FacesContext context, UIComponent component, Object value) throws ValidatorException {
-		if (! isPasswordValid((String) value)) {
-			FacesMessage msg = new FacesMessage("Password no valido.", "El password no coincide o no cumple el patrón requerido");
+
+	/*
+	 * Validador por edad. Debe ser mayor de 14 años
+	 */
+	public void ageValidator(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		if (Period.between(toLocalDate ((Date)value), toLocalDate(new Date())).getYears() <= 14) {
+			FacesMessage msg = new FacesMessage("Edad no válida",
+					"La edad mínima debe ser mayor de 14 años");
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
 			throw new ValidatorException(msg);
-		}		
+		}
 	}
 
-	private boolean isPasswordValid(String password) {		
-		Matcher matcher = pattern.matcher(password);
+	private LocalDate toLocalDate(Date date) {
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	public void passwordValidator(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		if (!isPasswordValid((String) value)) {
+			FacesMessage msg = new FacesMessage("Password no valido.",
+					"El password no coincide o no cumple el patrón requerido");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(msg);
+		}
+	}
+
+	private boolean isNameFormat(String name) {
+		Matcher matcher = namePattern.matcher(name);
+		return matcher.matches();
+	}
+
+	private boolean isPasswordValid(String password) {
+		Matcher matcher = passwpordPattern.matcher(password);
 		return matcher.matches();
 	}
 
